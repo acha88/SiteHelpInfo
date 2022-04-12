982f7ed74f66064d89851fdd846fefa9d219fed9
TheBeardGuru <al.atahiri@gmail.com>
2022-04-12 16:19:00 +0200

    css, calendar

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -15,10 +15,15 @@ class MailController extends AbstractController
      */
     public function sendEmail(MailerInterface $mailer): Response
     {
-        
-        
-              
+        // création du mail
+        $email = (new Email())
+            ->from($user->getUser()->getEmail())
+            ->to('carre.HelpInfo@free.fr')
+            ->subject('Confirmation rendez-vous de formation')
+            ->text('Madame, Monsieur, Merci de bien vouloir me confirmer mon créneau horaire choisi. Cordialement.');
+        // envoi du mail et confirmation   
         $mailer->send($email);
+        $this->addFlash('Un email de confirmation vous a été envoyé sur votre adresse de contact.');
         return $this->render('mail/index.html.twig', [
             'controller_name' => 'MailController',
         ]);
